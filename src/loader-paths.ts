709762c1bfaad1ba98aa86070ebf92a81dcf21c6
Vyacheslav Dorzhiev <14217809+sanperrier@users.noreply.github.com>
2024-09-30 10:48:48 +0400

    feat: `register()` for `--require` support

--- src/loader-paths.ts
+++ src/loader-paths.ts
@@ -5,8 +5,10 @@ import { fileURLToPath } from 'url'
 
 export const legacyLoader = getImportMetaURL('../esm/loader-legacy.mjs')
 export const importLoader = getImportMetaURL('../esm/import.mjs')
+export const registerRequire = getImportMetaURL('../commonjs/register.cjs')
 const legacyLoaderPath = fileURLToPath(legacyLoader)
 const importLoaderPath = fileURLToPath(importLoader)
+const registerRequirePath = fileURLToPath(registerRequire)
 
 export const legacyMatch = (p: string): boolean => {
   const d = p.startsWith('file://')
@@ -27,5 +29,16 @@ export const importMatch = (p: string): boolean => {
     d === '@tapjs/processinfo/import' ||
     p === importLoader ||
     resolve(d) === importLoaderPath
+  )
+}
+
+export const requireMatch = (p: string): boolean => {
+  const d = p.startsWith('file://')
+    ? fileURLToPath(p)
+    : decodeURIComponent(p);
+  return (
+    d === '@tapjs/processinfo/register' ||
+    p === registerRequire ||
+    resolve(d) === registerRequirePath
   )
 }
